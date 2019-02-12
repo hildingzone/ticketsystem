b2b1b562eb4af5cec88fa859925447654172c23e
Åsa H <asa.hildingsson@hotmail.com>
2019-02-12 13:43:54 +0100

    create start pages

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,31 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/events">Events</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #231f42;
 }
 #nav {
   padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: white;
+    text-decoration: none;
     &.router-link-exact-active {
-      color: #42b983;
+      color: rgb(108, 238, 255);
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Where it's @"/>
   </div>
 </template>
+
+
+
 
 <script>
 // @ is an alias to /src
