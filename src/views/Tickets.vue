ea4a7826d3c3dfaa81c399e640448d511f15444c
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-02-12 16:01:59 +0100

    Scratched the tickets.vue file

--- src/views/Tickets.vue
+++ src/views/Tickets.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="tickets">
-    <h1>Your tickets</h1>
+    <section class="what"></section>
+    <section class="where"></section>
+    <section class="when"></section>
+    <section class="from"></section>
+    <section class="to"></section>
+    <section class="info"></section>
+    <section class="code"></section>
   </div>
 </template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="sass">
+
+</style>
