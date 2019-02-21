06cafeecd294939656b5824b09bc3d6fca2d7764
Åsa H <asa.hildingsson@hotmail.com>
2019-02-21 12:24:15 +0100

    Update router.js

--- ticketsystem/src/router.js
+++ ticketsystem/src/router.js
@@ -4,36 +4,41 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import( './views/Home.vue')
-    },
-    {
-      path: '/tickets',
-      name: 'ticket',
-      component: () => import( './views/Tickets.vue')
-    },
-    {
-      path: '/events',
-      name: 'events',
-      component: () => import( './views/Events.vue')
-    },
-    {
-      path: '/buy',
-      name: 'buy',
-      component: () => import( './views/Buy.vue')
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: () => import( './views/Admin.vue')
-    },
-    {
-      path: '/verify',
-      name: 'verify',
-      component: () => import( './views/Verify.vue')
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: () =>
+                import ('./views/Home.vue')
+        },
+        {
+            path: '/tickets',
+            name: 'ticket',
+            component: () =>
+                import ('./views/Tickets.vue')
+        },
+        {
+            path: '/events',
+            name: 'events',
+            component: () =>
+                import ('./views/Events.vue')
+        },
+        {
+            path: '/buy',
+            name: 'buy',
+            component: () =>
+                import ('./views/Buy.vue')
+        },
+        {
+            path: '/admin',
+            name: 'admin',
+            component: () =>
+                import ('./views/Admin.vue')
+        },
+        {
+            path: '/verify',
+            name: 'verify',
+            component: () =>
+                import ('./views/Verify.vue')
+        }
+    ]
 })
